2582e7a6f4c08bdf8aa5a20904530c442c8e0931
jerry aaron <jerry.aaron45@gmail.com>
2019-08-26 21:22:55 +0100

    ajusted code

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 //create the book class
-class Book{
-    constructor(title,author,isbn){
-     this.title = title;
-     this.author = author;
-     this.isbn = isbn;
-    }
-}
+// class Book {
+//   constructor(title, author, isbn) {
+//     this.title = title;
+//     this.author = author;
+//     this.isbn = isbn;
+//   }
+// }
 
 //creating the interface class
-class Interface{
-
-}
+// class Interface {}
 
 //get the form and add event listener
-const form = document.querySelector('#form').addEventListener('submit', 
-function(e){
-    let title = document.querySelector('#title').value;
-        author = document.querySelector('#author').value;
-        isbn = document.querySelector('#isbn').value;
-console.log(title, author, isbn);
-e.preventDefault();        
-});
+const form = document.querySelector("#form");
+const tBody = document.querySelector("#t-body");
+const output = document.querySelector("#output");
+form.addEventListener("submit", addBooks);
+
+function addBooks(e) {
+  e.preventDefault();
+  let ul = document.createElement("ul");
+  ul.className = "output-list list-inline";
+  let attr = ["#title", "#author", "#isbn"];
+  attr.forEach(each => {
+    let li = document.createElement("li");
+    li.className = "list-inline-item";
+    li.textContent = document.querySelector(each).value;
+    ul.appendChild(li);
+    output.appendChild(ul);
+    console.log(ul);
+  });
+}
